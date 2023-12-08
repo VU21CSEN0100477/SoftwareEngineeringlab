5958363fd82bbb2fb40fa0426fcac2180452c1ff
VU21CSEN0100477 <93367794+VU21CSEN0100477@users.noreply.github.com>
2023-12-08 11:48:19 +0530

    Update fifth_hardcodedanduser.py

--- fifth_hardcodedanduser.py
+++ fifth_hardcodedanduser.py
@@ -6,16 +6,16 @@ b=int(input("Enter the value of b: "))
 c=int(input("Enter the value of c: "))
 x=np.linspace(-10, 10, 400)
 y1=a*x**2 + b*x + c
-plt.plot(x, y1, label=f'{a}x^2 + {b}x + {c}')
+plt.plot(x, y1, label=f'User Input Function')
 #Hardcoded
 a=1
 b=-3
 c=2
 y2=a*x**2+b*x+c
-plt.plot(x, y2, label=f'{a}x^2 + {b}x + {c}')
+plt.plot(x, y2, label=f'Hardcoded Function')
 plt.title('Plot of the quadratic functions')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.xlabel('Time')
+plt.ylabel('Temperature')
 plt.legend()
 plt.grid(True)
 plt.show()
