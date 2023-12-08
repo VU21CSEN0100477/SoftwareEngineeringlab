c8182e2fa8db2f189448d5209646c5c8e210a371
VU21CSEN0100477 <93367794+VU21CSEN0100477@users.noreply.github.com>
2023-12-08 11:46:45 +0530

    Update first_hardcodedvariables.py

--- first_hardcodedvariables.py
+++ first_hardcodedvariables.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-a = 1
-b = -3
+a = 4
+b = -8
 c = 2
 x = np.linspace(-10, 10, 400)
 y = a*x**2 + b*x + c
 plt.figure(figsize=(6, 4))
-plt.plot(x, y, label=f'{a}x^2 + {b}x + {c}')
+plt.plot(x, y, label=f'Harcoded variables')
 plt.title('Plot of the quadratic function')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.xlabel('Time')
+plt.ylabel('Temperature')
 plt.legend()
 plt.grid(True)
 plt.show()
